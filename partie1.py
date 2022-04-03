975724e3b0d4693be7834d8397d80085a77ba728
Remi Tang <remi.tang@epita.fr>
2022-04-03 23:42:38 +0200

    partie 1 OK

--- partie1.py
+++ partie1.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sympy import *
-import math
+from scipy.optimize import fsolve
 
 x = Symbol('x')
 pas = 0.1
@@ -23,9 +23,9 @@ def tracer(f, start, end):
 
 def monotonie(f, start, end):
     """Retourne 1 si croissant, -1 si décroissant, 0 sinon"""
-    tab = np.arange(start, end, pas) # [start, start + pas, ..., end]
-    derivee = [eval(d(f), t) for t in tab]
-    croissant = [element > 0 for element in derivee]
+    tab = np.arange(start, end + pas, pas) # [start, start + pas, ..., end]
+    derivee = [eval(d(f), t) for t in tab if eval(d(f), t).is_real]
+    croissant = [element >= 0 for element in derivee]
     if all(croissant): #Tous les éléments de croissants est True
         return 1
     elif not any(croissant): #Tous les éléments de croissants est True
@@ -34,13 +34,13 @@ def monotonie(f, start, end):
 
 def convexite(f, start, end):
     """Retourne 1 si convexe, -1 si concave, 0 sinon"""
-    return monotonie(d2(f), start, end)
+    return monotonie(d(f), start, end)
 
 def lagrange(f, start, end, erreur):
     if convexite(f, start, end) == 0:
         print("La fonction n'est ni concave ni convexe sur l'intervalle donné !")
-        return 0
-    if eval(f, start) * eval(d(f), start) > 0:
+        return 0, []
+    if eval(f, start) * eval(d2(f), start) > 0:
         pt_fixe, pt_pivot = start, end
     else:
         pt_fixe, pt_pivot = end, start
@@ -53,13 +53,12 @@ def lagrange(f, start, end, erreur):
         precision.append(xn)
     return pt_fixe, precision
 
-
 def newton(f, start, end, erreur):
     if convexite(f, start, end) == 0:
         print("La fonction n'est ni concave ni convexe sur l'intervalle donné !")
-        return 0
+        return 0, []
     if eval(f, start) * eval(d(f), start) > 0:
-        pt_fixe = star
+        pt_fixe = start
     else:
         pt_fixe = end
     x0 = pt_fixe
@@ -69,7 +68,7 @@ def newton(f, start, end, erreur):
         x0 = xn
         xn = xn - eval(f, xn) / eval(d(f), xn)
         precision.append(xn)
-    return pt_fixe, precision
+    return x0, precision
     
 def tracer_corde_lagrange(f, start, end, cordes):
     X = np.arange(start, end + pas, pas)
@@ -90,7 +89,7 @@ def tracer_corde_lagrange(f, start, end, cordes):
 def tracer_corde_newton(f, start, end, cordes):
     X = np.arange(start, end + pas, pas)
     Y = [ eval(f, x) for x in X ]
-    plt.title("Function " + str(f) + " avec méthode de Lagrange")
+    plt.title("Function " + str(f) + " avec méthode de Newton")
     plt.plot(X, Y)
 
     pt_fixe, precision = cordes
@@ -107,18 +106,76 @@ def tracer_corde_newton(f, start, end, cordes):
     plt.show()
 
 def ex(f, start, end, erreur):
-    print(f)
-    tracer(f, -1, 2)
+    print("Fonction :", f)
+    print("Solution :", [item.evalf() for item in solve(f, x) if item.is_real])
+    tracer(f, start, end)
 
     pt_f_L, precision_L  = lagrange(f, start, end, erreur)
     pt_f_N, precision_N  = newton(f, start, end, erreur)
 
+    print("Precision Lagrange : ", [float(item) for item in precision_L])
+    print("Precision Newton : ", [float(item) for item in precision_N])
+
     tracer_corde_lagrange(f, start, end, (pt_f_L, precision_L))
     tracer_corde_newton(f, start, end, (pt_f_N, precision_N))
+    print()
 
-    
 f1 = x**3 + x - 1
 f2 = x - exp(-x)
+f3 = 1 - x**2
 
 ex(f1, 0, 2, 0.01)
 ex(f2, 0, 1, 0.01)
+ex(f3, 0, 2, 0.01)
+
+def monotonie_intervalle(f, start, end):
+    """Retourne une liste d'intervalle croissant ou décroissant"""
+    croissant = []
+    decroissant = []
+    tab = np.arange(start, end + pas, pas)
+    derivee = [eval(d(f), t) for t in tab if eval(d(f), t).is_real]
+    check = [element >= 0 for element in derivee]
+    i = 0
+    while i < len(derivee):
+        croiss = []
+        decroiss = []
+        while i < len(check) and check[i]:
+            if (len(croiss) == 0):
+                croiss.append(tab[i])
+            i += 1
+        if (len(croiss) == 1):
+            croiss.append(tab[i - 1])
+        
+        while i < len(check) and not check[i]:
+            if (len(decroiss) == 0):
+                decroiss.append(tab[i])
+            i += 1
+        if (len(decroiss) == 1):
+            decroiss.append(tab[i - 1])
+        if len(decroiss) == 2:
+            decroissant.append(decroiss)
+        if len(croiss) == 2:
+            croissant.append(croiss)
+        i += 1
+    return croissant, decroissant
+
+def convexite_intervalle(f, start, end):
+    """Retourne une liste d'intervalle convexe ou concave"""
+    return monotonie_intervalle(d(f), start, end)
+
+def onlyNewton(f, start, end, erreur):
+    print("Fonction :", f)
+    print("Solution :", [item.evalf() for item in solve(f, x) if item.is_real])
+    tracer(f, start, end)
+
+    pt_f_N, precision_N  = newton(f, start, end, erreur)
+
+    print("Precision Newton : ", [float(item) for item in precision_N])
+    tracer_corde_newton(f, start, end, (pt_f_N, precision_N))
+    print()
+
+f4 = x**3 - 2 * x + 3
+
+print(monotonie_intervalle(f4, -50, 50))
+print(convexite_intervalle(f4, -50, 50))
+onlyNewton(f4, -10, -1, 0.1)
