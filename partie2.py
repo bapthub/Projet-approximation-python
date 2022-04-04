2c410fbebc26635a94aed30fffa9fd2dc53a80d1
Remi Tang <remi.tang@epita.fr>
2022-04-04 17:38:54 +0200

    Partie2 lagrange

--- partie2.py
+++ partie2.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sympy import *
 
 fig, ax = plt.subplots()
-ax.set(xlim=(0, 100), ylim=(0, 100))
+ax.set(xlim=(0, 10), ylim=(0, 10))
 ax.grid()
 
+x = Symbol('x')
+pas = 0.1
 coordonnee = []
 stop = False
+
+def eval(f, t):return f.subs(x, t)
 
 def onClick(event):
     try:
@@ -29,4 +34,30 @@ while True:
     if (stop):
         break
 
-print(coordonnee)
+def Lk(x, k, coordonnee):
+    result = 1
+    for i in range(len(coordonnee)):
+        if (i != k):
+            result *= (x - coordonnee[i][0]) / (coordonnee[k][0] - coordonnee[i][0])
+    return result
+
+def polynomeLagrange(x, coordonnee):
+    result = 0
+    for k in range(len(coordonnee)):
+        result += Lk(x, k, coordonnee) * coordonnee[k][1]
+    return result
+
+f = polynomeLagrange(x, coordonnee)
+
+def tracer(f, start, end):
+    X = np.arange(start, end + pas, pas)
+    Y = [ eval(f, x) for x in X ]
+    plt.title("Function " + str(f))
+    plt.plot(X, Y)
+    plt.axhline(0, color = "black")
+    plt.axvline(0, color = "black")
+    plt.grid()
+    plt.show()
+
+
+tracer(f, -10, 10)
